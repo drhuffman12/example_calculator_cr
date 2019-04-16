4cedf50f13db02212f1f9e35a6023cb34c585835
Daniel Huffman <drhuffman12@gmail.com>
2019-04-16 18:38:08 -0400

    add functions for all numbers and operations along with tests for the 4 examples

--- spec/example_calculator_spec.cr
+++ spec/example_calculator_spec.cr
@@ -1,18 +1,23 @@
 require "./spec_helper"
 
-def some_code
-  puts "some code not tested"
-end
-
 describe ExampleCalculator do
-  # TODO: Write tests
-
-  it "works" do
-    false.should eq(true)
+  it "has a version number" do
+    (ExampleCalculator::VERSION).should_not be nil
   end
 
-  # it "foo should return :bar" do
-  #   ExampleCalculator.foo.should eq(:bar)
-  # end
+  it "six(times(five)) # must return 30" do
+    six(times(five)).should eq(30)
+  end
 
+  it "one(plus(nine)) # must return 10" do
+    one(plus(nine)).should eq(10)
+  end
+
+  it "six(minus(three)) # must return 3" do
+    six(minus(three)).should eq(3)
+  end
+
+  it "eight(divided_by(two)) # must return 4" do
+    eight(divided_by(two)).should eq(4)
+  end
 end
